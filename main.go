7c8bb46bc7488e952a8c1a5db78565851b2b46da
Ellis Sinclair-Kent <ellissinclairkent@gmail.com>
2025-07-30 17:56:56 +0100

    CH1L4

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokédex >")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned_input := cleanInput(input)
+		first_word := cleaned_input[0]
+		fmt.Printf("Your command was: %v\n", first_word)
+	}
 }
 
 func cleanInput(text string) []string {
-	textSlice := strings.Fields(text)
+	lowercaseText := strings.ToLower(text)
+	textSlice := strings.Fields(lowercaseText)
 	return textSlice
 }
